df467a77faa55f4ade1e5f54b559d4aa088299ec
Isaac Colls <colls_isaac@hotmail.com>
2019-11-05 23:09:39 -0300

    dev db pass changed

--- docker-compose-lamp/www/test_db.php
+++ docker-compose-lamp/www/test_db.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("mysql", "root", "tiger", null);
+$link = mysqli_connect("mysql", "root", "cms420", null);
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
