6ec22e7cdf3976b0eb5bcc2b5e546eff458a39ff
Isaac Colls <colls_isaac@hotmail.com>
2019-11-17 11:35:03 -0300

    registrando suscriptores

--- cms/controllers/gestorMensajes.php
+++ cms/controllers/gestorMensajes.php
@@ -6,12 +6,6 @@ class MensajesController {
         if (isset($_POST["nombre"])) {
             if (preg_match('/^[a-zA-Z\s]+$/', $_POST["nombre"]) && preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $_POST["email"]) && preg_match('/^[a-zA-Z0-9\s\.,]+$/', $_POST["mensaje"])) {
 
-                $datosController = array(
-                    "nombre" => $_POST["nombre"],
-                    "email" => $_POST["email"],
-                    "mensaje" => $_POST["mensaje"]
-                );
-
                 // send email
                 // mail(to, subject, message, cabecera del correo)
                 // $correoDestino = "colls_isaac@yahoo.es";
@@ -25,9 +19,22 @@ class MensajesController {
                 // $envio = mail($correo, $asunto, $mensaje, $cabecera);
                 $envio = true;
 
+                // db
+                $datosController = array(
+                    "nombre" => $_POST["nombre"],
+                    "email" => $_POST["email"],
+                    "mensaje" => $_POST["mensaje"]
+                );
+                // almacenar en db el suscriptor
+                $datosSuscriptor = $_POST["email"];
+                $revisarSuscriptor = MensajesModel::revisarSuscriptorModel($datosSuscriptor, "suscriptores");
+                if (count($revisarSuscriptor["email"]) == 0) {
+                    MensajesModel::registroSuscriptoresModel($datosController, "suscriptores");
+                }
                 // almacenar en db el mensaje
                 $respuesta = MensajesModel::registroMensajesModel($datosController, "mensajes");
 
+                // show alert
                 if ($envio == true && $respuesta == "ok") {
                     echo '
                         <script>
--- cms/models/gestorMensajes.php
+++ cms/models/gestorMensajes.php
@@ -15,4 +15,28 @@ class MensajesModel {
         }
         $stmt -> close();
     }
+
+    // registro suscriptores
+    public function registroSuscriptoresModel($datos, $tabla) {
+        $stmt = Conexion::conectar() -> prepare("INSERT INTO $tabla (nombre, email) VALUES (:nombre, :email)");
+        $stmt -> bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
+        $stmt -> bindParam(":email", $datos["email"], PDO::PARAM_STR);
+
+        if ($stmt -> execute()) {
+            return "ok";
+        } else {
+            return "error";
+        }
+        $stmt -> close();
+    }
+
+    // validar suscriptor existente
+    public function revisarSuscriptorModel($datosModel, $tabla) {
+        $stmt = Conexion::conectar() -> prepare("SELECT email FROM $tabla WHERE email = :email");
+        $stmt -> bindParam(":email", $datosModel, PDO::PARAM_STR);
+        $stmt -> execute();
+        return $stmt -> fetch();
+        $stmt -> close();
+    }
+
 }
